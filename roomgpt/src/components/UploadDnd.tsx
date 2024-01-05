ee5313be3b47ba5613b8dbd26c1abf394a8a16a4
Dustin Yang <dustinyang@DustinucBookPro.lan>
2024-01-04 23:18:22 -0500

    update

--- roomgpt/src/components/UploadDnd.tsx
+++ roomgpt/src/components/UploadDnd.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import { UploadDropzone } from "@bytescale/upload-widget-react";
+import Image from "next/image";
 
 const UploadDnd = () => {
+ const [preview, setPreview] = useState<string>();
+  const [loading, setLoading] = useState<boolean>(true);
+//   const setImageUrl=useImage((state:any)=>state.setImageUrl)
   return (
     <div>UploadDnd</div>
   )
 }
 
 export default UploadDnd
+
+function useImage(arg0: (state: any) => any) {
+    throw new Error('Function not implemented.');
+}
